8dcd537a48838675463d455922103a48c30ca224
Osswa <ousswachouchane@aucegypt.edu>
2024-08-26 21:21:41 +0100

    changed gridDb with posgresql fixed Survey.java

--- src/main/java/com/osswa/yayaLimitusTest/DTO/SurveyDto.java
+++ src/main/java/com/osswa/yayaLimitusTest/DTO/SurveyDto.java
@@ -3,16 +3,18 @@ package com.osswa.yayaLimitusTest.DTO;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
 public class SurveyDto {
-    private String id;
+    private UUID id;
     private String title;
     private String description;
     private boolean isActive;
-    private Date createdAt;
+    private LocalDateTime createdAt;
     private List<QuestionDto> questions;
 }
--- src/main/java/com/osswa/yayaLimitusTest/Model/Survey.java
+++ src/main/java/com/osswa/yayaLimitusTest/Model/Survey.java
@@ -1,16 +1,81 @@
 package com.osswa.yayaLimitusTest.Model;
-import com.toshiba.mwcloud.gs.RowKey;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
-
-@Data
+@Entity
+@Table(name = "surveys")
 public class Survey {
-    @RowKey
+
+    //getters qnd setters
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private String title;
+
+    @Getter
+    @Column
+    private String description;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Setter
+    @Getter
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany (mappedBy = "survey", cascade =  CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Question> questions;
+
+    @OneToOne (mappedBy = "survey", cascade =  CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<SurveyResponse> surveyResponses;
+
+    //constructor
+    public Survey(){
+        this.title="Untitled survey";
+        this.description = "No description provided";
+        this.isActive = false;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Survey(String title, String description, boolean isActive){
+        this.title=title;
+        this.description=description;
+        this.isActive= isActive;
+        this.createdAt = LocalDateTime.now();
+    }
+    
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean is_active() {
+        return isActive;
+    }
+
+    public void set_active(boolean active) {
+        isActive=true;
+    }
+    
+    
+
+    /*
     String id;
     String title;
     String description;
     boolean is_active;
-    Date createdAt;
+    Date createdAt;*/
 }
